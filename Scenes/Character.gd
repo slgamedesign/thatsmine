61ea0291d4a8addb8664a405fd89e3652354a7e2
MaxGoxt <m.gomezcoitino@gmail.com>
2024-09-09 22:03:36 -0300

    Refactorizo código: - modularizo codigo en funciones - elimino código repetido

--- Scenes/Character.gd
+++ Scenes/Character.gd
@@ -6,34 +6,32 @@ extends Node2D
 @export var face : AnimatedSprite2D
 @export var hair : AnimatedSprite2D
 
+func random_color_clothes():
+	return Color(randf_range(0.0, 1.0), randf_range(0.0, 1.0), randf_range(0.0, 1.0))
+
+func random_color_hair():
+	return Color(randf_range(0.0, 1.0), randf_range(0.0, 1.0), randf_range(0.0, 1.0))
+
+func random_color_face():
+	return Color(randf_range(33.0/255.0, 250.0/255.0), randf_range(25.0/255.0, 226.0/255.0), randf_range(21.0/255.0, 212.0/255.0))
+
 # Se llama al ser instanciado el personaje, aleatoriza los 5 pelos, 5 caras, y 3 ropas. Además de los colores
 func _ready():
 	clothes.frame = randi_range(0, 2)
 	face.frame = randi_range(0, 4)
 	hair.frame = randi_range(0, 4)
-	clothes.modulate = Color(randf_range(0.0, 1.0), randf_range(0.0, 1.0), randf_range(0.0, 1.0))
-	var faceColor = Color(randf_range(33.0/255.0, 250.0/255.0), randf_range(25.0/255.0, 226.0/255.0), randf_range(21.0/255.0, 212.0/255.0))
+	clothes.modulate = random_color_clothes()
+	var faceColor = random_color_face()
 	face.modulate = faceColor
 	if hair.frame == 3:
 		hair.modulate = faceColor
 	else:
-		hair.modulate = Color(randf_range(0.0, 1.0), randf_range(0.0, 1.0), randf_range(0.0, 1.0))
+		hair.modulate = random_color_hair()
 	pass # Replace with function body.
 
 
 # PARA DEBUG (ALEATORIZA TODO NUEVAMENTE)
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
-		clothes.frame = randi_range(0, 2)
-		face.frame = randi_range(0, 4)
-		hair.frame = randi_range(0, 4)
-		clothes.modulate = Color(randf_range(0.0, 1.0), randf_range(0.0, 1.0), randf_range(0.0, 1.0))
-		var faceColor = Color(randf_range(33.0/255.0, 250.0/255.0), randf_range(25.0/255.0, 226.0/255.0), randf_range(21.0/255.0, 212.0/255.0))
-		face.modulate = faceColor
-		if hair.frame == 3:
-			hair.modulate = faceColor
-		else:
-			hair.modulate = Color(randf_range(0.0, 1.0), randf_range(0.0, 1.0), randf_range(0.0, 1.0))
+		_ready()
 	pass
-
-
