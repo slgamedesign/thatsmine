2326ffe82601f65414ba6882a6e20fc287f3dc20
MaxGoxt <m.gomezcoitino@gmail.com>
2024-09-12 10:20:42 -0300

    Cambio metodo de navegacion entre tabs

--- Scenes/ScreenManager.gd
+++ Scenes/ScreenManager.gd
@@ -3,36 +3,22 @@ extends Node2D
 @export var licTab : Node2D
 @export var infTab : Node2D
 
-var tabs
-var curTab : int
+var tabs : Array[Node]
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	tabs = Array(get_children(false))
-	print("size: ", tabs.size())
-	curTab = 0
-	to_tab(curTab)
-	pass # Replace with function body.
+	tabs = get_children(false)
+	_on_button_was_clicked("null", 0)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_button_was_clicked(buttonName : String, tab_index : int) -> void:
+	tabs.map(
+		func  (tab):
+			if tab == tabs[tab_index]:
+				tab.show()
+			else:
+				tab.hide()
 
-
-func _on_button_was_clicked(action: String, buttonName : String) -> void:
-	if action == "next":
-		if buttonName != "next":
-			global.sendInfo["license"] = buttonName
-		curTab += 1
-		to_tab(curTab)
-	elif action == "back":
-		curTab -= 1
-		to_tab(curTab)
-	print("license: ", global.sendInfo["license"], "\n")
-
-func to_tab(index : int):
-	for i in range(0, tabs.size()):
-		if i != index:
-			tabs[i].hide()
-		else:
-			tabs[i].show()
+			#if tab_index == 2:
+				#global.sendInfo["license"] = buttonName
+	)
