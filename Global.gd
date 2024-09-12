e87f25ac2130d1e8802f99bc69d6c2f0b2109623
MaxGoxt <m.gomezcoitino@gmail.com>
2024-09-12 02:27:34 -0300

    Refactorizo código

--- Global.gd
+++ Global.gd
@@ -1,4 +1,5 @@
 extends Node
 
 var isdragging = false
+var current_tab: int
 var sendInfo = {}
--- Scenes/ScreenManager.gd
+++ Scenes/ScreenManager.gd
@@ -3,32 +3,23 @@ extends Node2D
 @export var licTab : Node2D
 @export var infTab : Node2D
 
-var tabs
-var curTab : int
+var tabs : Array[Node]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	tabs = Array(get_children(false))
-	print("size: ", tabs.size())
-	curTab = 0
+	tabs = get_children(false)
+	global.current_tab = 0
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
-func _on_button_was_clicked(action: String, buttonName : String) -> void:
-	if action == "next":
-		if buttonName != "next":
-			global.sendInfo["license"] = buttonName
-		curTab += 1
-		to_inf_tab(curTab)
-	print(global.sendInfo["license"])
-
-func to_inf_tab(index : int):
-	for i in range(0, tabs.size()):
-		if i != index:
-			tabs[i].hide()
-		else:
-			tabs[i].show()
+func _on_button_was_clicked(_action: String, _buttonName : String, tab_index: int) -> void:
+	global.current_tab = tab_index
+	tabs.map(func(tab): 
+			if tab == tabs[tab_index] : 
+				tab.show() 
+			else: 
+				tab.hide())
