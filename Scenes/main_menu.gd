4bad6974baf7e3375cb7260eab08a48bab63d537
LuisPereira05 <luispereira.sl014@academico.ifsul.edu.br>
2024-09-25 21:38:36 -0300

    Main menu Intro

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -1,11 +1,15 @@
 extends Control
 
-
-
+@export var anim : AnimatedSprite2D
+@export var interface : MarginContainer
 func _on_play_pressed():
-	self.hide()
-	get_tree().change_scene_to_file("res://Scenes/Testing.tscn")
+	anim.play()
+	interface.hide()
 
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_fondo_animation_finished() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Testing.tscn")
